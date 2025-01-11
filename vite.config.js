10c17874cfcf112da26196ca138138f383b8b587
RamiroArambarri <raarambarri@gmail.com>
2025-01-11 20:15:49 -0300

    tercer commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/prueba-touch-música",
+  base: "/prueba-touch-musica",
   plugins: [react()],
 })
